99214f9390aaa9788b64d5bf3adf273d8b79c098
David McCann <david.a.mccann@gmail.com>
2011-08-18 14:56:17 +0300

    fix for gunicorn

--- emis_project/manage.py
+++ emis_project/manage.py
@@ -1,31 +1,19 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+# vim: ai ts=4 sts=4 et sw=4
 
-import sys, os
+#import sys, os
+
 from django.core.management import execute_manager
 import settings
 
-filedir = os.path.dirname(__file__)
-sys.path.append(os.path.join(filedir))
-sys.path.append(os.path.join(filedir,'rapidsms','lib'))
-sys.path.append(os.path.join(filedir,'rapidsms_auth'))
-sys.path.append(os.path.join(filedir,'rapidsms_contact'))
-sys.path.append(os.path.join(filedir,'rapidsms_cvs'))
-sys.path.append(os.path.join(filedir,'rapidsms_generic'))
-sys.path.append(os.path.join(filedir,'rapidsms_httprouter_src'))
-sys.path.append(os.path.join(filedir,'rapidsms_polls'))
-sys.path.append(os.path.join(filedir,'rapidsms_script'))
-sys.path.append(os.path.join(filedir,'rapidsms_uregister'))
-sys.path.append(os.path.join(filedir,'rapidsms_ureport'))
-sys.path.append(os.path.join(filedir,'rapidsms_xforms_src'))
-sys.path.append(os.path.join(filedir,'rapidsms_healthmodels'))
-sys.path.append(os.path.join(filedir,'django_eav'))
-sys.path.append(os.path.join(filedir,'rapidsms_logistics'))
-sys.path.append(os.path.join(filedir,'rapidsms_alerts'))
-sys.path.append(os.path.join(filedir,'..','lib','dimagi-utils'))
-sys.path.append(os.path.join(filedir,'uganda_common'))
-sys.path.append(os.path.join(filedir,'rapidsms_unregister'))
-sys.path.append(os.path.join(filedir,'rapidsms_emis'))
 
 if __name__ == "__main__":
+#    project_root = os.path.abspath(
+#        os.path.dirname(__file__))
+
+#    path = os.path.join(project_root, "apps")
+#    sys.path.insert(0, path)
+
+#    sys.path.insert(0, project_root)
     execute_manager(settings)
+
--- emis_project/settings.py
+++ emis_project/settings.py
@@ -3,6 +3,30 @@
 # encoding=utf-8
 
 # -------------------------------------------------------------------- #
+#                          PATH CONFIGURATION                          #
+# -------------------------------------------------------------------- #
+import sys, os
+filedir = os.path.dirname(__file__)
+sys.path.append(os.path.join(filedir))
+sys.path.append(os.path.join(filedir,'rapidsms','lib'))
+sys.path.append(os.path.join(filedir,'rapidsms_auth'))
+sys.path.append(os.path.join(filedir,'rapidsms_contact'))
+sys.path.append(os.path.join(filedir,'rapidsms_generic'))
+sys.path.append(os.path.join(filedir,'rapidsms_httprouter_src'))
+sys.path.append(os.path.join(filedir,'rapidsms_polls'))
+sys.path.append(os.path.join(filedir,'rapidsms_script'))
+sys.path.append(os.path.join(filedir,'rapidsms_uregister'))
+sys.path.append(os.path.join(filedir,'rapidsms_ureport'))
+sys.path.append(os.path.join(filedir,'rapidsms_xforms_src'))
+sys.path.append(os.path.join(filedir,'django_eav'))
+sys.path.append(os.path.join(filedir,'rapidsms_logistics'))
+sys.path.append(os.path.join(filedir,'rapidsms_alerts'))
+sys.path.append(os.path.join(filedir,'..','lib','dimagi-utils'))
+sys.path.append(os.path.join(filedir,'uganda_common'))
+sys.path.append(os.path.join(filedir,'rapidsms_unregister'))
+sys.path.append(os.path.join(filedir,'rapidsms_emis'))
+
+# -------------------------------------------------------------------- #
 #                          MAIN CONFIGURATION                          #
 # -------------------------------------------------------------------- #
 TIME_ZONE="Africa/Kampala"
