eb1c7239ebef590545d060b9bf80d53db49a44d5
David McCann <david.a.mccann@gmail.com>
2011-08-27 20:54:28 +0300

    settings change

--- emis_project/settings.py
+++ emis_project/settings.py
@@ -46,8 +46,9 @@ CATEGORY_COLORS = ['#AA4643', '#4572A7', '#89A54E', '#80699B', '#3D96AE', '#DB84
 DATABASES = {
     'default': {
         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-	'NAME': 'emis',
-        'USER': 'www-data',
+	'NAME': 'rapidsms-test',
+        'USER': 'postgres',
+        'HOST': 'dbserver',
     }
 }
 # the rapidsms backend configuration is designed to resemble django's
